95b3b81edf50852badb82df548f8fe91bf070da5
sumitbisht <sumitbisht1987@gmail.com>
2014-02-15 13:36:16 +0530

    added image parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 
 var app = express();
+var ncr = require('nodecr');
+var path = require('path');
+var fs = require('fs');
 app.set('view engine', 'jade');
 
 app.configure(function(){
@@ -9,11 +12,25 @@ app.configure(function(){
 	app.use(express.session());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(express.bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}));
 	app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res){
 	res.render("index");
+});
+
+app.post('/process', function(req, res){
+	var filePath = req.files.pic.path;
+	var text = '';
+	ncr.process(filePath, function(error, text){
+		if(error){
+			console.log('Error occured while parsing image for text: '+error);
+			res.redirect('/');
+		}
+		console.log('Here is the text: '+text);
+		res.redirect('/success?text='+text);
+	});
 });
 
 app.use(app.router);
